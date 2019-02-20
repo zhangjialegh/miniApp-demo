74d9a728c04d32f6b7888a092c4210285401a6ad
zhangjialegh <zjl1195185765@outlook.com>
2019-02-20 09:14:12 +0800

    fix-inteval

--- controller/news.js
+++ controller/news.js
@@ -4,18 +4,10 @@ exports.newsTopic = async ctx => {
   const { lastCursor=0, pageSize=20 } = ctx.request.query
     try {
       const res = await newsModel.getTopic(lastCursor, pageSize)
-      const data = res.map((item) => {
-        let ele = item
-        ele['inteval'] = Math.ceil((Date.now() - new Date(item.publish_time).getTime())/1000/60)
-      console.log(ele['inteval'],'fdsfsfs');
-
-        return ele
-      })
-      
       ctx.body = {
         code: 200,
         success: true,
-        data: data
+        data: res
       }
     } catch (error) {
       console.log(error)
